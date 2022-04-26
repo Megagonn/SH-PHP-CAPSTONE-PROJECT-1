2117d153bd1211c1bdc3f2f3e4f18d3091ff33b6
Meg <yusuffrasheed.yr@gmail.com>
2022-04-26 15:01:49 +0100

    chess board

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chess Board</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+<h1>Chess Board</h1>
+    <table border="1px" class="table table-border mx-auto my-auto" style="width: 400px; height: 400px;">
+        <?php
+        $_chess_box_count = 8;
+        $chess_box = '';
+        $chess_row = '';
+        $alt_color = true;
+        for ($i = 1; $i <= $_chess_box_count; $i++) {
+            if ($i%2 == 0) {
+                $chess_box = "<td class='bg-secondary' style='width: 30px; height: 30px;'>".$chess_box."</td>";
+            }
+            else{
+                $chess_box = "<td style='width: 30px; height: 30px;'>".$chess_box."</td>";
+            }
+        }
+        for ($i = 0; $i < $_chess_box_count; $i++) {
+            $chess_row = "<tr>" . $chess_box . "</tr>";
+            echo $chess_row;
+        }
+        ?>
+    </table>
+</body>
+
+</html>
