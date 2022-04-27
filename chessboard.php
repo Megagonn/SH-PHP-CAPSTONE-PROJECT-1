6adeadc68fac0dba814c3cd38e186f49a34c8d6b
Meg <yusuffrasheed.yr@gmail.com>
2022-04-27 16:30:59 +0100

    final commit

---
+++ chessboard.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chess Board</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+<h1>Chess Board</h1>
+    <table border="1px" class="table table-bordered mx-auto my-auto" style="width: 400px; height: 400px;">
+        <?php
+        $_chess_box_count = 8;
+        $chess_box = '';
+        $chess_row = '';
+        $alt_color = true;
+        for ($i = 1; $i <= $_chess_box_count; $i++) {
+            $chess_box = "<td style='width: 30px; height: 30px;'>".$i."</td>".$chess_box."";
+            for ($j = 0; $j < $i; $j++) {
+            $chess_row = "<tr>" . $chess_box . "</tr>";
+            }
+        }
+        echo $chess_row;
+        ?>
+    </table>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,40 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chess Board</title>
+    <title>Group 50</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
-
 <body>
-<h1>Chess Board</h1>
-    <table border="1px" class="table table-bordered mx-auto my-auto" style="width: 400px; height: 400px;">
-        <?php
-        $_chess_box_count = 8;
-        $chess_box = '';
-        $chess_row = '';
-        $alt_color = true;
-        for ($i = 1; $i <= $_chess_box_count; $i++) {
-            $chess_box = "<td style='width: 30px; height: 30px;'>".$i."</td>".$chess_box."";
-            // if ($i%2 == 0) {
-            // }
-            // else{
-            //     $chess_box = "<td style='width: 30px; height: 30px;'></td>".$chess_box."";
-            // }
-            for ($j = 0; $j < $i; $j++) {
-            $chess_row = "<tr>" . $chess_box . "</tr>";
-            }
-        }
-        echo $chess_row;
-        // for ($i = 0; $i < $_chess_box_count; $i++) {
-        //     $chess_row = "<tr>" . $chess_box . "</tr>";
-        //     echo $chess_row;
-        // }
-        ?>
-    </table>
+<div class="m-1 rounded bg-info d-flex" style="min-height: 98vh; max-height:98vh">
+        <div class="w-50"></div>
+        <div class="w-50 px-5 py-1 bg-light" >
+            <div class="w-100 rounded border border-danger my-5 mx-auto d-flex p-5">
+                <button type="submit" class="btn btn-outline-info w-100 me-1"><a href="./chessboard.php">Chess Board</a></button><br><br>
+                <button type="submit" class="btn btn-outline-success w-100"><a href="./bill.php">Electricity Bill Calculator</a></button>
+            </div>
+        </div>
+    </div>
 </body>
-
 </html>
